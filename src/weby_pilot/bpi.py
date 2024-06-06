27cb5e6caa95a2f961efbcb85b46e856c7a834d5
João Magalhães <joamag@gmail.com>
2024-06-07 00:25:56 +0100

    chore: finalized invoice download support

--- src/weby_pilot/bpi.py
+++ src/weby_pilot/bpi.py
@@ -41,6 +41,15 @@ SelectDateRange = Literal[
     "Intervalo de Datas",
 ]
 
+DocumentType = Literal[
+    "Todos",
+    "Avisos",
+    "Factura-Recibo",
+    "Facturas",
+    "Notas de Crédito",
+    "Notas de Débito",
+]
+
 
 class BpiAPI(WebyAPI):
     @classmethod
@@ -55,13 +64,20 @@ class BpiAPI(WebyAPI):
         return username, password
 
     @classmethod
-    def download_invoice(cls, range: SelectDateRange = "Último Ano"):
+    def download_invoice(
+        cls,
+        date_range: SelectDateRange = "Último Ano",
+        document_type: DocumentType = "Facturas",
+        invoice_indexes: Sequence[int] = (0,),
+    ):
         instance = cls()
         with instance.driver_ctx(options=cls.build_options()):
             instance.login(*cls.build_login())
             instance.select_section("Consultas")
             instance.select_side_menu("Avisos/Faturas/Notas Crédito e Débito")
-            instance.select_item(instance.get_elements(By.XPATH, "//select")[2], range)
+            instance.select_filters(date_range=date_range, document_type=document_type)
+            for invoice_index in invoice_indexes:
+                instance.click_extract(row_index=invoice_index)
 
     @classmethod
     def download_report(
@@ -100,32 +116,39 @@ class BpiAPI(WebyAPI):
         close = self.driver.find_element(By.ID, "fechar")
         close.click()
 
-        username_e = self.get_element(By.XPATH, '//*[@label="Nome Acesso"]')
-        password_e = self.get_element(By.XPATH, '//*[@label="Código Secreto"]')
+        username_e = self.get_element(By.XPATH, "//*[@label='Nome Acesso']")
+        password_e = self.get_element(By.XPATH, "//*[@label='Código Secreto']")
         username_e.send_keys(username)
         password_e.send_keys(password)
         password_e.send_keys(Keys.RETURN)
 
     def select_section(self, section: BpiSections):
-        section_e = self.get_element(By.XPATH, f'//a[contains(text(), "{section}")]')
+        section_e = self.get_element(By.XPATH, f"//a[contains(text(), '{section}')]")
         section_e.click()
 
     def select_side_menu(self, side_section: BpiSideSections):
         side_section_e = self.get_element(
-            By.XPATH, f'//div[contains(text(), "{side_section}")]'
+            By.XPATH, f"//div[contains(text(), '{side_section}')]"
         )
         side_section_e.click()
+
+    def select_filters(self, date_range: SelectDateRange, document_type: DocumentType):
+        self.select_item(self.get_elements(By.XPATH, "//select")[2], date_range)
+        self.select_item(self.get_elements(By.XPATH, "//select")[3], document_type)
+
+        filter = self.get_element(By.XPATH, "//*[@value='Filtrar']")
+        filter.click()
 
     def click_extract(self, row_index=0, wait_download: bool = True):
         open_extract = self.get_element(
             By.XPATH,
-            f'//table[contains(@class, "TableRecords")]//tr[{row_index + 1}]//a[contains(text(), "Abrir")]',
+            f"//table[contains(@class, 'TableRecords')]//tr[{row_index + 1}]//a[contains(text(), 'Abrir')]",
         )
         with self.download_ctx(wait_download=wait_download):
             open_extract.click()
 
     def click_card_account(self, row_index=0, wait_download: bool = True):
         card_account = self.get_element(
-            By.XPATH, f'//a[contains(@class, "Text_NoWrap")][{row_index + 1}]'
+            By.XPATH, f"//a[contains(@class, 'Text_NoWrap')][{row_index + 1}]"
         )
         card_account.click()
