15a57b1be798e442a177f737ddd82f454811b90b
João Magalhães <joamag@gmail.com>
2024-06-07 00:26:29 +0100

    BPI APi support (#3)
    * chore: initial driver structure
    
    * chore: improved API structure
    
    * chore: elegant API for get element and download
    
    * chore: much improved API and report download
    
    * chore: support for card reports
    
    * chore: more generic report extraction
    
    * chore: initial download invoice support
    
    Lacks stable support for the select component.
    
    * chore: finalized invoice download support

---
+++ src/weby_pilot/__main__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from .bpi import BpiAPI
+
+# BpiAPI.download_report(report_indexes=range(0, 8))
+
+# BpiAPI.download_report(section="Extrato Investimento", report_indexes=range(0, 8))
+
+BpiAPI.download_invoice()
+
+# BpiAPI.download_card_report(report_indexes=range(0, 8))
