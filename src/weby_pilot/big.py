d6fcd8cb30593749e42cbe4d725352e116e40438
João Magalhães <joamag@gmail.com>
2024-06-07 12:34:33 +0100

    chore: small fixes

--- src/weby_pilot/big.py
+++ src/weby_pilot/big.py
@@ -2,19 +2,20 @@
 # -*- coding: utf-8 -*-
 
 from os.path import environ
+from typing import Tuple
 
 from .base import WebyAPI
 
 class BpiAPI(WebyAPI):
     @classmethod
-    def build_login(cls) -> Tuple[str, str]:
+    def build_login(cls) -> Tuple[str, str, str]:
         username = environ.get("BIG_USERNAME", None)
         password = environ.get("BIG_PASSWORD", None)
         nif = environ.get("BIG_NIf", None)
         if username is None:
-            raise Exception("BPI_USERNAME must be set")
+            raise Exception("BIG_USERNAME must be set")
         if password is None:
-            raise Exception("BPI_PASSWORD must be set")
+            raise Exception("BIG_PASSWORD must be set")
         if nif is None:
             raise Exception("BIG_NIF must be set")
 
